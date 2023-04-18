baf8084e4247e86f2dc9086d481d7e47d8aa987c
prakhar <autogear070@gmail.com>
2023-04-18 13:16:19 +0530

    first commit

---
+++ client/src/store/index.js
@@ -0,0 +1,12 @@
+import { proxy } from 'valtio';
+
+const state = proxy({
+  intro: true,
+  color: '#EFBD48',
+  isLogoTexture: true,
+  isFullTexture: false,
+  logoDecal: './nike.png',
+  fullDecal: './threejs.png',
+});
+
+export default state;
